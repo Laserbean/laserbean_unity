1802f6186ba53bcbffe19e2a1e772d005c227b04
laserbean <laserbean00001@gmail.com>
2023-12-19 21:46:44 +0800

    fixed annoying thing with custom dict.

--- DataAndSaving/CustomDict.cs
+++ DataAndSaving/CustomDict.cs
@@ -19,7 +19,7 @@ namespace Laserbean.SpecialData
 {
 
     [System.Serializable]
-    public class CustomDictionary <Tkey, Tvalue>: ICollection<KeyValuePair<Tkey, Tvalue>>, IEnumerable<KeyValuePair<Tkey, Tvalue>>,
+    public class CustomDictionary<Tkey, Tvalue> : ICollection<KeyValuePair<Tkey, Tvalue>>, IEnumerable<KeyValuePair<Tkey, Tvalue>>,
         // IEnumerable, 
         IDictionary<Tkey, Tvalue>, IReadOnlyCollection<KeyValuePair<Tkey, Tvalue>>, IReadOnlyDictionary<Tkey, Tvalue>,
         // ICollection, 
@@ -29,20 +29,20 @@ namespace Laserbean.SpecialData
 
     {
 
-        public List<SerializedKeyValuePair<Tkey, Tvalue>> keyValueList = new(); 
+        public List<SerializedKeyValuePair<Tkey, Tvalue>> keyValueList = new();
 
         [NonSerialized]
-        public List<Tkey> keys = new(); 
+        public List<Tkey> keys = new();
         [NonSerialized]
-        public List<Tvalue> values = new(); 
-        
+        public List<Tvalue> values = new();
+
         public ICollection<Tkey> Keys => keys;
         public ICollection<Tvalue> Values => values;
 
-        public Tvalue this[Tkey key] { 
-            get => values[keys.IndexOf(key)]; 
+        public Tvalue this[Tkey key] {
+            get => values[keys.IndexOf(key)];
             set {
-                if (keys.Contains(key)){
+                if (keys.Contains(key)) {
                     values[keys.IndexOf(key)] = value;
                 } else {
                     values.Add(value);
@@ -66,11 +66,11 @@ namespace Laserbean.SpecialData
 
         public void Add(Tkey key, Tvalue value)
         {
-            if (Keys.Contains(key)) throw new ArgumentException(); 
-            if (key == null) throw new ArgumentNullException(); 
+            if (Keys.Contains(key)) throw new ArgumentException();
+            if (key == null) throw new ArgumentNullException();
 
             Keys.Add(key);
-            values.Add(value); 
+            values.Add(value);
         }
 
         public void Clear()
@@ -81,7 +81,7 @@ namespace Laserbean.SpecialData
 
         public bool Contains(KeyValuePair<Tkey, Tvalue> item)
         {
-            return keys.Contains(item.Key) && values.Contains(item.Value); 
+            return keys.Contains(item.Key) && values.Contains(item.Value);
         }
 
         public bool ContainsKey(Tkey key)
@@ -89,10 +89,10 @@ namespace Laserbean.SpecialData
             return keys.Contains(key);
         }
 
-            public void CopyTo(KeyValuePair<Tkey, Tvalue>[] array, int arrayIndex)
-            {
-                throw new NotImplementedException();
-            }
+        public void CopyTo(KeyValuePair<Tkey, Tvalue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
 
         public IEnumerator<KeyValuePair<Tkey, Tvalue>> GetEnumerator()
         {
@@ -101,155 +101,147 @@ namespace Laserbean.SpecialData
             }
         }
 
-        void UpdateKeysAndValues() {
+        void UpdateKeysAndValues()
+        {
             keys.Clear();
-            values.Clear(); 
-            foreach(var kvp in keyValueList) {
+            values.Clear();
+            foreach (var kvp in keyValueList) {
                 if (keys.Contains(kvp.Key))
                     Debug.LogError("Dictionary already contains key: " + kvp.Key);
-                
+
                 keys.Add(kvp.Key);
-                values.Add(kvp.Value); 
+
+                if (kvp.Value != null) {
+                    values.Add(kvp.Value);
+                } else {
+                    if (typeof(Tvalue).IsValueType && !typeof(Tvalue).IsClass) {
+                        values.Add(default(Tvalue));
+                    } else {
+                        values.Add(Activator.CreateInstance<Tvalue>());
+                    }
+                }
+
             }
         }
-            public void OnAfterDeserialize()
-            {
-                UpdateKeysAndValues();
+        void ISerializationCallbackReceiver.OnAfterDeserialize()
+        {
+            UpdateKeysAndValues();
+        }
+
+        void ISerializationCallbackReceiver.OnBeforeSerialize()
+        {
+            keyValueList.Clear();
+            for (int i = 0; i < Count; i++) {
+                keyValueList.Add(new SerializedKeyValuePair<Tkey, Tvalue>(keys[i], values[i]));
             }
 
-            public void OnBeforeSerialize()
-            {
-                keyValueList.Clear();
-                for (int i = 0; i < Count; i++) {
-                    keyValueList.Add (new SerializedKeyValuePair<Tkey, Tvalue>(keys[i], values[i]));
-                }     
+        }
 
-            }
-
-            public void OnDeserialization(object sender)
-            {
-                UpdateKeysAndValues();
-            }
+        public void OnDeserialization(object sender)
+        {
+            UpdateKeysAndValues();
+        }
 
         public bool Remove(KeyValuePair<Tkey, Tvalue> item)
         {
-            int index = keys.IndexOf(item.Key); 
+            int index = keys.IndexOf(item.Key);
 
-            if (index < 0) return false; 
+            if (index < 0) return false;
 
             keys.RemoveAt(index);
-            values.RemoveAt(index); 
+            values.RemoveAt(index);
 
-            return true; 
+            return true;
         }
 
         public bool Remove(Tkey key)
         {
-            int index = keys.IndexOf(key); 
+            int index = keys.IndexOf(key);
 
-            if (index < 0) return false; 
+            if (index < 0) return false;
 
             keys.RemoveAt(index);
-            values.RemoveAt(index); 
+            values.RemoveAt(index);
 
-            return true; 
+            return true;
         }
 
         public bool TryGetValue(Tkey key, out Tvalue value)
-        { 
+        {
             value = default;
-            if (!keys.Contains(key)) 
-                return false; 
-            
-            value = values[keys.IndexOf(key)]; 
-            return true; 
+            if (!keys.Contains(key))
+                return false;
+
+            value = values[keys.IndexOf(key)];
+            return true;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             for (int i = 0; i < Count; i++) {
                 yield return new KeyValuePair<Tkey, Tvalue>(keys[i], values[i]);
-            }        
+            }
         }
     }
 
-[System.Serializable]
-public struct SerializedKeyValuePair<TKey, TValue>
-{
-    [field: SerializeField]
-    public TKey Key {set; get; }
+    [System.Serializable]
+    public struct SerializedKeyValuePair<TKey, TValue>
+    {
+        [field: SerializeField]
+        public TKey Key { set; get; }
 
-    [field: SerializeField]
-    public TValue Value {set; get; }
+        [field: SerializeReference]
+        public TValue Value { set; get; }
 
-    public SerializedKeyValuePair(TKey key, TValue val) {
-        Key = key;
-        Value = val; 
+        public SerializedKeyValuePair(TKey key)
+        {
+            Key = key;
+            Value = default;
+        }
+
+        public SerializedKeyValuePair(TKey key, TValue val)
+        {
+            Key = key;
+            Value = val;
+        }
     }
-}
 
 
 #if UNITY_EDITOR
 
 
 
-// // [CustomPropertyDrawer(typeof(CustomDictionary<,>))]
-// // public class CustomDictionaryProperty : PropertyDrawer 
-// // {
-// //     const string propname = "keyValueList";
+    // [CustomPropertyDrawer(typeof(CustomDictionary<,>))]
+    // public class CustomDictionaryProperty : PropertyDrawer 
+    // {
+    //     const string propname = "keyValueList";
 
-// //     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-// //     {
-// //         EditorGUI.BeginProperty(position, label, property);
+    //     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    //     {
+    //         base.OnGUI(position, property, label);
+    //         EditorGUI.BeginProperty(position, label, property);
 
-// //         // Calculate the height of a single line
-// //         float lineHeight = EditorGUIUtility.singleLineHeight;
+    //         // Calculate the height of a single line
+    //         float lineHeight = EditorGUIUtility.singleLineHeight;
 
-// //         // Draw a label for the list
+    //         SerializedProperty keyValuePairs = property.FindPropertyRelative(propname);
 
-// //         // EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+    //         EditorGUI.PropertyField(position, keyValuePairs, label, true);
 
+    //         // if (EditorGUI.B)
+    //         // EditorGUI.LabelField(position, "Fish");
+    //         EditorGUI.EndProperty();
 
-// //         SerializedProperty keyValuePairs = property.FindPropertyRelative(propname);
-
-
-// //         EditorGUI.PropertyField(position, keyValuePairs, label, true);
-
-
-// //         // EditorGUI.LabelField(position, "Fish");
-// //         EditorGUI.EndProperty();
-
-// //     }
+    //     }
 
 
-// //     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
-// //     {
-// //         float lineHeight = EditorGUIUtility.singleLineHeight;
-// //         float totalheight = 0f; 
-
-// //         totalheight += lineHeight*1; 
-
-// //         SerializedProperty keyValuePairs = property.FindPropertyRelative(propname);
-
-// //         if (!keyValuePairs.isExpanded) {
-// //             return totalheight; 
-// //         }
-
-// //         totalheight += lineHeight*3; 
-
-// //         for (int i = 0; i < keyValuePairs.arraySize; i++)
-// //         {
-// //             SerializedProperty pair = keyValuePairs.GetArrayElementAtIndex(i);
-// //             // totalheight += pair.isExpanded ? lineHeight * 3 : lineHeight; 
-// //             totalheight += EditorGUI.GetPropertyHeight(pair);
-// //         }
-        
-
-// //         return totalheight;
-// //     }
+    //     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    //     {
+    //         return base.GetPropertyHeight(property, label) + 10f; 
+    //     }
 
 
-// // }
+    // }
 
 
 

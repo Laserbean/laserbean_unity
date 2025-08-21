8f35065f27b721d9f917529c088ec7378690c184
Laserbean <laserbean00001@gmail.com>
2025-08-21 22:13:43 +1200

    just removed the set d to zero

--- Follow/FollowTransform.cs
+++ Follow/FollowTransform.cs
@@ -6,11 +6,6 @@ namespace Laserbean.General.Follower
     public class FollowTransform : SmoothFollow
     {
 
-        void Awake()
-        {
-            d_const = 0;
-        }
-
         public override void AddForce(Vector3 force)
         {
             base.AddForce(force);

75e3396cb3b2cb354f26e0e132a200d18ad6f55a
Laserbean <laserbean00001@gmail.com>
2026-03-19 17:34:46 +1300

    todo maybe

---
+++ Interfaces/InterfaceMonobehaviour.cs
@@ -0,0 +1,17 @@
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+// using UnityEngine.Events;
+// using System.Collections.Generic;
+
+// namespace Laserbean.General.Interfaces
+// {
+
+//     public class InterfaceMonobehaviour<T> : MonoBehaviour
+//     {
+//         List<T> interfaceList = new(); 
+
+
+
+//     }
+
+// }

9fda9f132bb9b313f6318e2b6445a3679e4019fa
Laserbean <laserbean00001@gmail.com>
2026-03-19 17:33:04 +1300

    vector extension

--- Extensions/VectorExtensions.cs
+++ Extensions/VectorExtensions.cs
@@ -359,6 +359,22 @@ namespace Laserbean.General
             }
         }
 
+        public static Vector3 SetX(this Vector3 v, float value)
+        {
+            return new(value, v.y, v.z);
+        }
+
+        public static Vector3 SetY(this Vector3 v, float value)
+        {
+            return new(v.x, value, v.z);
+        }
+
+        public static Vector3 SetZ(this Vector3 v, float value)
+        {
+            return new(v.x, v.y, value);
+        }
+
+
 
         public static Vector3 Average(this List<Vector3> Targets)
         {
